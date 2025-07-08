8a2b13bc5fa2e3c72e941fdcc86113a30e0e5107
Vinay RS <vinay.ramesh@appsndevices.in>
2025-07-08 11:16:15 +0530

    Image insertion functionality working properly for desired cursor position and Latex formulas are getting inserted properly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import JoditEditorWithLatex from './components/JoditEditorWithLatex/JoditEditorWithLatex';
+import Fixed from './components/Fixed/Fixed';
 
 function App() {
   return (
     <div className="App">
       <JoditEditorWithLatex />
+      {/* <Fixed /> */}
     </div>
   );
 }
